6a78dfc731826f9b17ea0b88842ce5c10d40ce36
zhensg123 <xyqblog@163.com>
2021-05-28 22:51:13 +0800

    '提交'

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import store from './store'
 import '@/assets/styles/reset.css'
 import '@/element'
 
+import * as filters from './filters' // global filters
+import * as directives from './directives' // global directives
+import '@/components' // 导入全局组件
+
+Object.keys(filters).forEach(key => Vue.filter(key, filters[key]))
+Object.keys(directives).forEach(key => Vue.directive(key, directives[key]))
 Vue.config.productionTip = false
 
 new Vue({
