06855ba532d0b5d54acefe3cffbe7df0c37c3c68
zhensg123 <xyqblog@163.com>
2021-05-28 22:11:20 +0800

    '提交'

---
+++ src/views/home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <el-button @click="testApi">测试api请求</el-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import homeApi from './api/Home'
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    testApi () {
+      const params = {
+        test: 122
+      }
+      homeApi.getTestApi(params).then((res) => {
+        console.log(res, 'ress')
+      })
+    }
+  }
+}
+</script>
